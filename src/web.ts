27e77473ebceaeeb4707af0b9f08e8b7d9dd2a40
Dave Alden <dpa99c@gmail.com>
2025-06-11 08:12:35 +0100

    (web) bugfix: HTMLAudioElement volume is not reliable, so use our data in preference

--- src/web.ts
+++ src/web.ts
@@ -59,7 +59,7 @@ export class NativeAudioWeb extends WebPlugin implements NativeAudio {
     const audio: HTMLAudioElement = this.getAudioAsset(options.assetId).audio;
     this.cancelGainNodeRamp(audio); // cancel any existing scheduled volume changes
     const data = this.getAudioAssetData(options.assetId);
-    data.volumeBeforePause = audio.volume;
+    data.volumeBeforePause = data.volume || audio.volume;
     this.setAudioAssetData(options.assetId, data);
 
     if (options?.fadeOut) {
